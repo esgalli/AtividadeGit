18e369914648efa830a21d702195afc5a7bb9542
juanprimo1 <juan_primo123@hotmail.com>
2024-06-12 19:34:48 -0300

    Adicionado a estrutura da classe Pedido

---
+++ TecnicasRefatoracao/R01.ExtractMethod/antes/Pedido.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TecnicasRefatoracao.R01.ExtractMethod.antes
+{
+    public class Pedido
+    {
+        
+    }
+}
