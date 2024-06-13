f8605a8a423c0c63dafaf943bb4c17113d508810
Ribeiro <joao.ribeiro17@outlook.com>
2024-06-12 21:41:05 -0300

    Adiciona estrutura da classe Pedido

--- TecnicasRefatoracao/R01.ExtractMethod/antes/Relatorio.cs
+++ TecnicasRefatoracao/R01.ExtractMethod/antes/Relatorio.cs
@@ -35,29 +35,6 @@ namespace refatoracao.Aula01.R01.ExtractMethod.antes
             Console.WriteLine("valor: " + total);
         }
     }
-
-    class Pedido
-    {
-        private readonly IList<Item> itens = new List<Item>();
-        public IReadOnlyCollection<Item> Itens
-        {
-            get { return new ReadOnlyCollection<Item>(itens); }
-        }
-
-        private readonly string cliente;
-        public string Cliente { get => cliente; }
-
-        public Pedido(string cliente)
-        {
-            this.cliente = cliente;
-        }
-
-        public void AddItem(string descricao, int quantidade, decimal precoBase, decimal acrescimo, decimal desconto)
-        {
-            itens.Add(new Item(descricao, quantidade, precoBase, acrescimo, desconto));
-        }
-    }
-
     class Item
     {
         private readonly string descricao;
