a613d1b9c7248664005358288fe4ad8b63f8254d
Everton Galli <everton.sgalli@gmail.com>
2024-06-19 21:31:38 -0300

    Correcao classe Pedido

--- TecnicasRefatoracao/R01.ExtractMethod/antes/Pedido.cs
+++ TecnicasRefatoracao/R01.ExtractMethod/antes/Pedido.cs
@@ -7,24 +7,27 @@ using refatoracao.Aula01.R01.ExtractMethod.antes;
 
 namespace TecnicasRefatoracao.R01.ExtractMethod
 {
-    class Pedido
+    publi class Pedido
     {
         private readonly IList<Item> itens = new List<Item>();
-        public IReadOnlyCollection<Item> Itens
-        {
-            get { return new ReadOnlyCollection<Item>(itens); }
-        }
-
         private readonly string cliente;
-        public string Cliente { get => cliente; }
 
         public Pedido(string cliente)
         {
             this.cliente = cliente;
         }
 
+        public string Cliente { get => cliente; }
+
+        public IReadOnlyCollection<Item> Itens
+        {
+            get { return new ReadOnlyCollection<Item>(itens); }
+        }
+ 
         public void AddItem(string descricao, int quantidade, decimal precoBase, decimal acrescimo, decimal desconto)
         {
             itens.Add(new Item(descricao, quantidade, precoBase, acrescimo, desconto));
         }
+
+    }
 }
